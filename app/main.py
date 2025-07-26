1e05c66ab94bc7e1fa70adecb1a0d82bdbe76235
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-27 05:46:07 +0600

    feat: add an api endpoint for query and getting answer

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
+from app.routes.v1.endpoints import ask
 
+app = FastAPI(
+    title="Multilingual RAG System",
+    version="1.0",
+    description="A system that answers questions in multiple languages using a RAG model.",
+)
+
+app.include_router(ask.router, prefix="/api/v1")
