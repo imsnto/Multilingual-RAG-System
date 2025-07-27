841156ddb8521943e7a6202c4dc12521ac88979a
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-27 09:43:33 +0600

    feat: add functionality of english query
    - added english query service sothat system can generate response both eng and ban
    - updated readme.md file
    - updated requirements.txt file
    - showed screenshots of responses

--- app/routes/v1/endpoints/ask.py
+++ app/routes/v1/endpoints/ask.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, Field
+from langdetect import detect
 
 from app.config import settings 
 from app.utils.vector_store import PineconeManager
 from app.utils.rag_chain import RAGChainBuilder
-
+from app.services.english_query_service import process_prompt
 
 router = APIRouter()
 
@@ -13,11 +14,18 @@ class QueryRequest(BaseModel):
     query: str = Field(..., example="কাকে অনুপমের ভাগ্য দেবতা বলে উল্লেখ করা হয়েছে?", description="The question to be answered", title="User Query")
 
 class QueryResponse(BaseModel):
+    query: str
     answer: str
 
 @router.post("/ask", response_model=QueryResponse)
 async def ask_question(request: QueryRequest):
-    print(request.query)
+    lang = detect(request.query)
+    
+    query = request.query
+    if lang == 'en':
+        response = process_prompt(request.query)
+        query = response['response']
+
     pinecone_manager = PineconeManager(settings)
     try:
         vector_store_instance = pinecone_manager.get_vector_store()
@@ -25,8 +33,13 @@ async def ask_question(request: QueryRequest):
         rag_chain = rag_builder.build_rag_chain()
 
 
-        response = rag_chain.invoke(request.query)
-        return QueryResponse(answer=response)
+        response = rag_chain.invoke(query)
+        if lang == 'en':
+            response = process_prompt(response)['response']
+        return QueryResponse(
+            query=request.query, 
+            answer=response
+            )
 
     except Exception as e:
         raise HTTPException(status_code=500, detail="Could not run RAG chain test. Ensure Pinecone index is populated and API keys are correct.")
